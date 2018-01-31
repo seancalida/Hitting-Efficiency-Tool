b62c4fdb89223386424a14622d7319ed98ebe006
Sean Calida <seanc830@vt.edu>
2018-01-30 20:16:42 -0500

    Project runner class

---
+++ StatRunner.java
@@ -0,0 +1,17 @@
+package offenseStatTrack;
+
+/**
+ * Project runner for the application
+ * 
+ * @author Sean Calida
+ * @version 1/30/18
+ */
+public class StatRunner {
+	
+	/*
+	 * Main method for the project
+	 */
+	public static void main(String[] args) {
+		new PreGameGUI();
+		}
+	}
